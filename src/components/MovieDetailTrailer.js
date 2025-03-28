679c4ea7a3b12c5f1d11d4690ccc5cbeb853314e
DarjiHet <hetdarjibca22@gmail.com>
2025-03-29 01:42:37 +0530

    final changes

--- src/components/MovieDetailTrailer.js
+++ src/components/MovieDetailTrailer.js
@@ -7,7 +7,7 @@ const MovieDetailTrailer = ({ id }) => {
     return (
         <div>
             <iframe
-                className="w-[50rem] aspect-video"
+                className="w-[25rem] md:w-[50rem] aspect-video"
                 src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&rel=0"} //+ "?&autoplay=1&mute=true"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
